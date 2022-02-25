dd8986f4ea57992ba594c7e77602d8debcd2b065
xbdirisxk <abdirisakjr@gmail.com>
2022-02-25 22:54:44 +0300

    double adding fields

--- src/components/AddNoteField.jsx
+++ src/components/AddNoteField.jsx
@@ -4,21 +4,35 @@ import styled from 'styled-components';
 const AddNoteField = () => {
     return (
         <InputField>
-            <input type='text' placeholder='Take a note...' />
+            <div className='text-inputs'>
+                <input type='text' placeholder='title' />
+                <input type='text' placeholder='Take a note...' />
+            </div>
         </InputField>
     );
 };
 
 const InputField = styled.div`
+    display: flex;
+    justify-content: center;
     font-size: 1.2rem;
     padding: 5px;
     width: 80%;
 
-    input {
+    .text-inputs {
         width: 80%;
-        border: 1px;
         border-radius: 8px;
         box-shadow: 0 1px 3px 2px lightgray;
+        padding: 5px;
+    }
+
+    input:first-of-type {
+        width: 100%;
+        padding: 10px 15px;
+    }
+    input:last-of-type {
+        font-size: 1rem;
+        width: 100%;
         padding: 10px 15px;
     }
 `;
