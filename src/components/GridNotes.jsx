a6ede7e442c8022e63816b76aca7da326a32d385
xbdirisxk <abdirisakjr@gmail.com>
2022-02-26 07:05:32 +0300

    display notes in the masonty grid

--- src/components/GridNotes.jsx
+++ src/components/GridNotes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/masonry-grid.css';
 import Masonry from 'react-masonry-css';
 
-const GridNotes = () => {
+const GridNotes = ({ notes }) => {
     const breakpointColumnsObj = {
         default: 3,
         1100: 3,
@@ -15,33 +15,12 @@ const GridNotes = () => {
             className='my-masonry-grid'
             columnClassName='my-masonry-grid_column'
         >
-            <div>
-                <h4>Lorem ipsum dolor</h4>
-                Lorem ipsum dolor sit amet,consectetur adipisicing elit. Lorem
-                veniam
-            </div>
-
-            <div>
-                <h4>ipsum dolor sit amet</h4>
-                Lorem ipsum dolor sit amet,consectetur adipisicing elit. Lorem
-                ipsum dolor sit amet, consectetur adipisicing elit. Nisi amet
-                neque, asperiores non saepe dicta omnis rem officiis alias
-                veniam
-            </div>
-            <div>
-                <h4>Lorem ipsum</h4>
-                Lorem ipsum dolor sit amet,consectetur adipisicing elit. Lorem
-                ipsum dolor sit amet, consectetur adipisicing elit. Nisi amet
-                neque, asperiores non saepe dicta omnis rem officiis alias
-                veniam Lorem ipsum dolor sit amet,consectetur adipisicing elit.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
-                amet neque, asperiores non saepe dicta omnis rem officiis alias
-                veniam
-            </div>
-            <div>
-                <h4>ipsum</h4>
-                ipsum dolor sit amet, consectetur adipisicing elit.
-            </div>
+            {notes.map((note) => (
+                <div>
+                    <h4>{note.title}</h4>
+                    <p>{note.body}</p>
+                </div>
+            ))}
         </Masonry>
     );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainContent from './styles/MainContent.styled';
 import AddNoteField from './AddNoteField';
 import GridNotes from './GridNotes';
 
 const Main = () => {
+    const [notes, setNotes] = useState([
+        {
+            title: 'sleeping early',
+            body: 'as my mom says if you sleep early then you will wake up early',
+        },
+    ]);
     return (
         <MainContent>
-            <AddNoteField />
-            <GridNotes />
+            <AddNoteField setNotes={setNotes} />
+            <GridNotes notes={notes} />
         </MainContent>
     );
 };
