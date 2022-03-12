a7b9660ff7dbb7c64c97d615935cbf1b5a1925bd
xbdirisxk <abdirisakjr@gmail.com>
2022-03-12 23:44:03 +0300

    unarchive notes

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -5,11 +5,28 @@ import { ArchiveContext } from '../context/NotesProvider';
 import { MdOutlineUnarchive } from 'react-icons/md';
 import { IoTrashOutline } from 'react-icons/io5';
 import { ThemeContext } from '../App';
+import { NotesContext, TrashContext } from '../context/NotesProvider';
 
 const ArchiveNotes = () => {
-    const [archiveNote] = useContext(ArchiveContext);
     const [themeType] = useContext(ThemeContext);
+    const [archiveNote, setArchiveNote] = useContext(ArchiveContext);
+    const [notes, setNotes] = useContext(NotesContext);
+    const [trashNote, setTrashNote] = useContext(TrashContext);
 
+    const unArchive = (id) => {
+        const item = archiveNote.filter((note) => note.id === id)[0];
+        const updateArchive = archiveNote.filter((note) => note.id !== id);
+        setArchiveNote(updateArchive);
+        setNotes([...notes, item]);
+    };
+
+    const moveToTrash = (id) => {
+        console.log('note:', notes);
+        const item = notes.filter((note) => note.id === id)[0];
+        const updateNotes = notes.filter((note) => note.id !== id);
+        setNotes(updateNotes);
+        setTrashNote([...trashNote, item]);
+    };
     return (
         <Grid themeType={themeType}>
             {archiveNote.map((note) => (
@@ -18,11 +35,14 @@ const ArchiveNotes = () => {
                         <h4>{note.title}</h4>
                         <p>{note.body}</p>
                     </div>
-                    <div className='icons'>
+                    <div className='icons' onClick={() => unArchive(note.id)}>
                         <span>
                             <MdOutlineUnarchive />
                         </span>
-                        <span className='delete-icon'>
+                        <span
+                            className='delete-icon'
+                            onClick={() => moveToTrash(note.id)}
+                        >
                             <IoTrashOutline />
                         </span>
                     </div>
